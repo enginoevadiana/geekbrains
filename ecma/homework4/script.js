e801cb60fc89234df222bb3abe66f0c640eb4c1d
Diana <diana@MBP-Diana.Dlink>
2023-11-25 01:30:37 +0300

    ecma hw 4 completed

--- ecma/homework4/script.js
+++ ecma/homework4/script.js
@@ -1,12 +1,19 @@
 const usersContainer = document.querySelector('.usersContainer');
 let usersList;
 
-if (localStorage.getItem('usersList') === null) {
+if (localStorage.getItem('usersList') === null || localStorage.getItem('usersList') === undefined) {
+	// console.log('localstorage is not here');
 	loadUsers();
 }
 else {
+	// console.log('localstorage here');
 	usersList = JSON.parse(localStorage.getItem('usersList'));
-	showUsersList(usersList);
+	if (usersList.length === 0) {
+		loadUsers();
+	}
+	else {
+		showUsersList(usersList);
+	}
 }
 
 async function fetchUsers() {
@@ -14,7 +21,7 @@ async function fetchUsers() {
 		const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
 		const users = await response.json();
 		localStorage.setItem('usersList', JSON.stringify(users));
-		console.log('fetchUsers is working');
+		// console.log('fetchUsers is working');
 		return users;
 	} catch (error) {
 		console.error('Ошибка при загрузке фотографий:', error);
@@ -28,6 +35,7 @@ async function loadUsers() {
 }
 
 function showUsersList(usersList) {
+	usersList = JSON.parse(localStorage.getItem('usersList'));
 	usersContainer.textContent = '';
 	usersList.forEach(user => {
 		let userItem = document.createElement('div');
@@ -54,23 +62,25 @@ function showUsersList(usersList) {
 		deleteButton.textContent = 'Delete';
 		userItem.appendChild(deleteButton);
 	});
+	makeUserDeleted(usersList);
 }
 
-let deleteButton = document.querySelectorAll('.deleteButton');
+function makeUserDeleted(usersList) {
+	let deleteButton = document.querySelectorAll('.deleteButton');
 
-deleteButton.forEach(item => {
-	item.addEventListener('click', event => {
-		let deletedUserName = event.target.parentNode.firstChild.textContent;
-		console.log('deletedUserName ', deletedUserName);
-		console.log(usersList);
-		let deletedUser = usersList.find((user) => { return user.name === deletedUserName; }); console.log(deletedUser, ' deleted user');
+	deleteButton.forEach(item => {
+		item.addEventListener('click', event => {
+			let deletedUserName = event.target.parentNode.firstChild.textContent;
+			let deletedUser = usersList.find((user) => { return user.name === deletedUserName; });
 
-		let deletedUserId = usersList.indexOf(deletedUser);
+			let deletedUserId = usersList.indexOf(deletedUser);
 
-		usersList.splice(deletedUserId, 1);
-		localStorage.setItem('usersList', JSON.stringify(usersList));
-		showUsersList(usersList);
+			usersList.splice(deletedUserId, 1);
+			localStorage.setItem('usersList', JSON.stringify(usersList));
+			showUsersList(usersList);
+		});
 	});
-});
+}
+
 
 // Необходимо получить список всех пользователей с помощью бесплатного API (https://jsonplaceholder.typicode.com/users) и отобразить их на странице. Пользователь должен иметь возможность удалить любого пользователя из списка. Данные при получении необходимо сохранить в локальное хранилище браузера localStorage. При удалении пользователь должен удаляться не только со страницы, но и из локального хранилища localStorage
