920d7d9ace4bc335939ea03499fe32a12fe832af
Diana <diana@MBP-Diana.Dlink>
2023-12-01 21:06:58 +0300

    homework 3 ecma added

---
+++ ecma/homework3/task2.js
@@ -0,0 +1,60 @@
+// Управление списком заказов (использование классов)
+
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+// Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// Пример использования класса
+// class Product {
+// constructor(name, price) {
+// this.name = name;
+// this.price = price;
+// }
+// }
+
+// const order = new Order(12345);
+
+// const product1 = new Product(""Phone"", 500);
+// order.addProduct(product1);
+
+// const product2 = new Product(""Headphones"", 100);
+// order.addProduct(product2);
+
+// console.log(order.getTotalPrice()); // Вывод: 600
+
+class Order {
+	orderNumber;
+	products = [];
+	totalPrice = 0;
+
+	constructor() {
+	}
+	addProduct(product) {
+		this.products.push(product);
+	}
+	getTotalPrice() {
+		this.products.forEach(product => {
+			this.totalPrice = this.totalPrice + product.price;
+		});
+		return this.totalPrice;
+	}
+}
+
+class Product {
+	constructor(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
