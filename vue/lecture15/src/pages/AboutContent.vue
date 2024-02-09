827d6f85000f3fca8bb329a6c3e8079213b127b6
Diana <diana@MacBook-Pro-Diana.local>
2024-02-09 13:30:20 +0300

    lecture 15

---
+++ vue/lecture15/src/pages/AboutContent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		AboutContent
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AboutContent',
+
+	data() {
+		return {
+
+		};
+	},
+
+	mounted() {
+
+	},
+
+	methods: {
+
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue/lecture15/src/pages/NotFound.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		NotFound
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NotFound',
+
+	data() {
+		return {
+
+		};
+	},
+
+	mounted() {
+
+	},
+
+	methods: {
+
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
